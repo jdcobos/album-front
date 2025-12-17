6b7deea902ba0f0da52c0d997fa6109da09b3aea
jdcobos <jcobos1999@gmail.com>
2025-12-17 16:15:42 -0500

    component muro de fotos

--- src/components/photoWall/index.tsx
+++ src/components/photoWall/index.tsx
@@ -1,7 +1,35 @@
+import { useDispatch, useSelector } from "react-redux"
+import type { AppDispatch, RootState } from "../../store"
+import { isEmpty } from "ramda"
+import { GET_MULTIMEDIA } from "../../actions/multimedia.actions"
+import { useEffect } from "react"
+import '../../stylesheet/photoWall/photoWall.scss'
+
 const PhotoWall = () => {
+    const dispatch = useDispatch<AppDispatch>()
+      const { loading, multimedia } = useSelector((state: RootState) => state.multimedia)
+    
+      useEffect(()=>{
+         if(isEmpty(multimedia)){
+            dispatch(GET_MULTIMEDIA())
+         }
+      },[multimedia]
+    )
     return (
-        <div className="">
-            muro 
+     <div className="photoWall">
+        <>
+            {!loading ? (
+            <>
+                {multimedia.map((item) => (
+                <div className="photoWall_card">
+                    <img src={item?.src} />
+                </div>
+                ))}
+            </>
+            ) : (
+            "cargando..."
+            )}
+        </>
         </div>
     );
 }
